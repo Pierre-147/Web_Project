8a6c69db41d2d8b5419a6c244810641066de161d
Laetitia <laetitia.husson@student.yncrea.fr>
2020-10-03 13:09:05 +0200

    Page créer un compte

---
+++ CreateAccount.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+        <head>           
+            <link href='CACss.css' rel='stylesheet'>
+        </head>
+
+
+    <body>
+
+        <h1> Créez votre compte </h1>
+        
+        <form method="post" action="CreateAccount.php">
+         <br />
+            <label> Username : </label>
+            <input type="text" placeholder="username"/><br /><br />
+            <label> Mail : </label>
+            <input type="text" placeholder="e-mail"/><br /><br />
+            <label> Password :</label>
+            <input type="text" placeholder="password"/><br /><br />
+            <label> Confirme password : </label>
+            <input type="text" placeholder="confirme password"/><br /><br />
+       
+            <input type="submit" value="Valider" />
+         <br /><br />  
+        
+       
+        </form>
+
+
+      
+      
+            
+        
+        
+    
+
+        
+
+    </body>
+</html>
