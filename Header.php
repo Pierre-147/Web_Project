b4a9cb7fb9f55b8e38571d3576f707faeb76e390
Pierre <pierre.niclas@student.yncrea.fr>
2020-10-07 18:02:56 +0200

    first version of header and footer, no CSS yet

--- Header.php
+++ Header.php
@@ -1,5 +1,11 @@
-<?php
-echo("coucou");
-?>
-
-hello
+<header>
+    <a href = "homepage.php">Le bazard des films</a>
+    <nav>
+        <span>Nos Quizzs</span>
+        <div>
+            <a href = "insert">Quizz 1</a>
+            <a href = "insert">Quizz 2</a>
+        </div>
+        <a href = 'insert'>Votre conte</a>
+    </nav>
+</header>
