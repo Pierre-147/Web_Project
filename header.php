2cde7c77e024cc0c1311ef70546dc04fb59d9b8d
CyrilDelannoy <cyril.delannoy@student.yncrea.fr>
2020-10-09 16:04:03 +0200

    quiz2 v2

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <div id = 'drop'>Nos Quiz
             <nav class = "child">
                 <a href = "quiz1.php">Quiz 1</a>
-                <a href = "quizpage2">Quiz 2</a>
+                <a href = "quiz2.php">Quiz 2</a>
             </nav>
         </div>
         <a href = 'CreateAccount.php'>Votre compte</a>
