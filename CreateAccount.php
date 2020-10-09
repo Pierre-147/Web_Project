8575e4a3a29a405ff6987cd527f6ea5f36d56d79
Laetitia <laetitia.husson@student.yncrea.fr>
2020-10-09 15:53:23 +0200

    Ajout connection

--- CreateAccount.php
+++ CreateAccount.php
@@ -8,23 +8,45 @@
     </head>
     <body>
         <?php include('homepage.php') ?>
+        <div class='formulaire'>
 
-        <h1> Créez votre compte !</h1>
-        
-        <form method="post" action="CreateAccount.php">
-            <br />
-            <label> Pseudo : </label>
-            <input type="text" placeholder="Pseudo"/><br /><br />
-            <label> E-Mail : </label>
-            <input type="mail" placeholder="e-mail"/><br /><br />
-            <label> Mot de passe :</label>
-            <input type="password" placeholder="mot de passe"/><br /><br />
-            <label> Confirmer votre mot de passe : </label>
-            <input type="password" placeholder="confirmer votre mot de passe"/><br /><br />
-       
-            <input type="submit" class= bouton value="Valider" />
-            <br />
-            <br />  
-        </form>
+            <div class='Creation'>
+                <h1> Créez votre compte !</h1>
+                
+                <form method="post" action="CreateAccount.php">
+                    <br />
+                    <label> Pseudo : </label>
+                    <input type="text" placeholder="Pseudo"/><br /><br />
+                    <label> E-Mail : </label>
+                    <input type="mail" placeholder="e-mail"/><br /><br />
+                    <label> Mot de passe :</label>
+                    <input type="password" placeholder="mot de passe"/><br /><br />
+                    <label> Confirmer votre mot de passe : </label>
+                    <input type="password" placeholder="confirmer votre mot de passe"/><br /><br />
+            
+                    <input type="submit" class= bouton value="Valider" />
+                    <br />
+                    <br />  
+                </form>
+            </div>
+
+            <div class='Connection'>
+                <h1> Connecter vous !</h1>
+
+                <form method="post" action="CreateAccount.php">
+                    <br />
+                    <label> Pseudo : </label>
+                    <input type="text" placeholder="Pseudo"/><br /><br />
+                    
+                    <label> Mot de passe :</label>
+                    <input type="password" placeholder="mot de passe"/><br /><br />           
+                    
+                    <input type="submit" class= bouton value="Valider" />
+                    <br />
+                    <br />  
+                </form>
+            </div>
+        </div>
+
     </body>
 </html>
