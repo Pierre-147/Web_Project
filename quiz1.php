f69a67c4ddb300b207bfa1afae7684bc154e66d7
Michael <michael.gross@student.yncrea.fr>
2020-10-21 11:47:48 +0200

    quiz avec tableau

--- quiz1.php
+++ quiz1.php
@@ -9,45 +9,59 @@
     <body>
         <?php include('header.php');?>
         <div id="page-container">
-            <h3>Quiz 1: Star Wars</h3>
+            <?php
+                $title = "Star Wars";
+                $tabQuestion = array(
+                    "Combien de Star Wars à realisé Georges Lucas ?",
+                    "Lesquels de ces personnages ne sont pas des Jedi ?",
+                    "Dans La Revanche des Siths, quel adversaire affronte Maitre Yoda ?",
+                    "Quel est l'année de sortie du premier Star Wars ?",
+                );
+                $tabLabelReponse = array(
+                    array(6, 9, 11),
+                    array("Yoda", "Jar Jar Binks", "R2D2", "Ahsoka Tano"),
+                    array("Dark Plageis", "Dark Maul", "Dark Sidious"),
+                );
+            ?>
+            <h3>Quiz 1: <?php echo($title);?></h3>
             <br/>
             <form action="reponseQuiz1.php" method="GET">
-                <label id="question" for="q1">Question 1 : Combien de Star Wars à realisé Georges Lucas ?</label> 
+                <label id="question" for="q1">Question 1 : <?php echo($tabQuestion[0]);?></label> 
                 <p>
                     <div id="reponsePossible">
-                        <label>6</label>
+                        <label><?php echo($tabLabelReponse[0][0]);?></label>
                         <input type="radio" id="q1" name="q1" value="rep1">
-                        <label>9</label>
+                        <label><?php echo($tabLabelReponse[0][1]);?></label>
                         <input type="radio" id="q1" name="q1" value="rep2">
-                        <label for="q1">11</label>
+                        <label><?php echo($tabLabelReponse[0][2]);?></label>
                         <input type="radio" id="q1" name="q1" value="rep3">
                     </div>
                 </p>
-                <label id="question" for="q2">Question 2 : Lesquels de ces personnages ne sont pas des Jedi ?</label> 
+                <label id="question" for="q2">Question 2 : <?php echo($tabQuestion[1]);?></label> 
                 <p>
                     <div id="reponsePossible">
-                        <label for="q2">Yoda</label>
+                        <label for="q2"><?php echo($tabLabelReponse[1][0]);?></label>
                         <input type="checkbox" name="q2a">
-                        <label for="q2">Jar Jar Binks</label>
+                        <label for="q2"><?php echo($tabLabelReponse[1][1]);?></label>
                         <input type="checkbox" name="q2b">
-                        <label for="q2">R2D2</label>
+                        <label for="q2"><?php echo($tabLabelReponse[1][2]);?></label>
                         <input type="checkbox" name="q2c">
-                        <label for="q2">Ahsoka Tano</label>
+                        <label for="q2"><?php echo($tabLabelReponse[1][3]);?></label>
                         <input type="checkbox" name="q2d">
                     </div>
                 </p>
-                <label id="question" for="q3">Question 3 : Dans La Revanche des Siths, quel adversaire affronte Maitre Yoda ?</label> 
+                <label id="question" for="q3">Question 3 : <?php echo($tabQuestion[2]);?></label> 
                 <p>
                     <div id="reponsePossible">
                         <select name="q3">
                             <option value="">--Please choose an option--</option>
-                            <option value="rep1">Dark Plagueis</option>
-                            <option value="rep2">Dark Maul</option>
-                            <option value="rep3">Dark Sidious</option>
+                            <option value="rep1"><?php echo($tabLabelReponse[2][0]);?></option>
+                            <option value="rep2"><?php echo($tabLabelReponse[2][1]);?></option>
+                            <option value="rep3"><?php echo($tabLabelReponse[2][2]);?></option>
                         </select>
                     </div>
                 </p>
-                <label id="question" for="q4">Question 4 : Quel est l'année de sortie du premier Star Wars ?</label> 
+                <label id="question" for="q4">Question 4 : <?php echo($tabQuestion[3]);?></label> 
                 <p>
                     <div id="reponsePossible">
                        <input type="number" id="q4" name="q4" placeholder="2000" min="1800" max="2020"> 
