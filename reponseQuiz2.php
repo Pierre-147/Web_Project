a1fd8f42c3eca8a404c68ac4998ceb8c1f369910
Chopineau Maxence <maxence.chopineau@yncrea.fr>
2020-10-23 15:48:51 +0200

    only one mechanism verification

--- reponseQuiz2.php
+++ reponseQuiz2.php
@@ -13,65 +13,7 @@
         <?php include('header.php');?>
         <h3>Quiz 2: Quentin Tarantino</h3>
         <br/>
-        <label id="question">Question 1 : Dans le film "Pulp fiction", où meurt le personnage incarné par John Travolta ?</label>
-        <div id="reponse">
-            <?php
-            if (isset($_GET['q1']))
-            {
-                if($_GET['q1'] === "rep3q1")
-                {
-                    echo("Bonne réponse");
-                }
-                else
-                {
-                    echo("Faux la réponse était : Aux toilettes");
-                } 
-            }
-            else
-            {
-                echo("Faux la réponse était : Aux toilettes");
-            }         
-            ?>
-        </div>
-        <label id="question">Question 2 : Parmi ces personnages, quels sont ceux qui meurent dans le film "Django unchained" ?</label>
-        <div id="reponse">
-            <?php
-            if (!(isset($_GET['q2a'])) && isset($_GET['q2b']) && isset($_GET['q2c']) && !(isset($_GET['q2d'])))
-            {
-                echo("Bonne réponse");
-            }
-            else
-            {
-                echo("Faux les réponses étaient : Dr King Schultz et Calvin Candie");
-            }
-            ?>
-        </div>
-        <label id="question">Question 3 : Quel est le 1er film réalisé par Quentin Tarantino ?</label>
-        <div id="reponse">
-            <?php
-            if($_GET['q3'] === "rep4q3")
-            {
-                echo("Bonne réponse");
-            }
-            else
-            {
-                echo("Faux la réponse était : Reservoir Dogs");
-            }
-            ?>
-        </div>
-        <label id="question">Question 4 : Combien de films Quentin Tarantino a-t-il rélisé ?</label>
-        <div id="reponse">
-            <?php
-            if($_GET['q4'] === "9")
-            {
-                echo("Bonne réponse");
-            }
-            else
-            {
-                echo("Faux la réponse était : 9");
-            }
-            ?>
-        </div>
+        <?php include('checkAnswers.php');?>
         <?php include('footer.php');?>
     </body>
 </html>
