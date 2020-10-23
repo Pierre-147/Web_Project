771e8d919bf7934efd73b93c6da4c7629a3a66aa
Michael <michael.gross@student.yncrea.fr>
2020-10-23 16:00:53 +0200

    maj homepage
    on enleve les tests bdd

--- homepage.php
+++ homepage.php
@@ -1,12 +1,4 @@
-<?php include('ouverture.php'); 
-$req = "SELECT answer_text FROM answer WHERE answer_question_id = 2";
-$res=$database->query($req);
-while ($donnee=$res->fetch()){
-    echo($donnee[0]);
-}
-    
-
-?>
+<?php include('ouverture.php');?>
 
 <html>
 <head>
