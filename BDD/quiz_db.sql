298adcb0b8e5efa90d671710d0d22161c06c88f2
Michael <michael.gross@student.yncrea.fr>
2020-11-04 17:27:06 +0100

    maj BDD

--- BDD/quiz_db.sql
+++ BDD/quiz_db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : ven. 23 oct. 2020 à 14:06
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Host: localhost
+-- Generation Time: Nov 04, 2020 at 05:26 PM
+-- Server version: 8.0.22-0ubuntu0.20.04.2
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,26 +19,24 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `quiz_db`
+-- Database: `quiz_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `answer`
+-- Table structure for table `answer`
 --
 
-DROP TABLE IF EXISTS `answer`;
-CREATE TABLE IF NOT EXISTS `answer` (
-  `answer_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'answer identifier',
+CREATE TABLE `answer` (
+  `answer_id` int NOT NULL COMMENT 'answer identifier',
   `answer_text` varchar(255) NOT NULL COMMENT 'text of the answer',
   `is_valid_answer` tinyint(1) NOT NULL COMMENT 'valid answer for question',
-  `answer_question_id` int(11) NOT NULL COMMENT 'question related',
-  PRIMARY KEY (`answer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+  `answer_question_id` int NOT NULL COMMENT 'question related'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `answer`
+-- Dumping data for table `answer`
 --
 
 INSERT INTO `answer` (`answer_id`, `answer_text`, `is_valid_answer`, `answer_question_id`) VALUES
@@ -73,48 +72,43 @@ INSERT INTO `answer` (`answer_id`, `answer_text`, `is_valid_answer`, `answer_que
 -- --------------------------------------------------------
 
 --
--- Structure de la table `question`
+-- Table structure for table `question`
 --
 
-DROP TABLE IF EXISTS `question`;
-CREATE TABLE IF NOT EXISTS `question` (
-  `question_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'question_identification',
+CREATE TABLE `question` (
+  `question_id` int NOT NULL COMMENT 'question_identification',
   `question_title` varchar(255) NOT NULL COMMENT 'title of the question',
-  `question_quizz_id` int(11) NOT NULL COMMENT 'link question quizz',
-  `question_input_type` varchar(255) NOT NULL COMMENT 'input of the question',
-  PRIMARY KEY (`question_id`),
-  KEY `question_quizz_id_fk` (`question_quizz_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+  `question_quizz_id` int NOT NULL COMMENT 'link question quizz',
+  `question_input_type` varchar(255) NOT NULL COMMENT 'input of the question'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `question`
+-- Dumping data for table `question`
 --
 
 INSERT INTO `question` (`question_id`, `question_title`, `question_quizz_id`, `question_input_type`) VALUES
-(1, 'Combien de Star Wars a realise Georges Lucas ? ', 1, 'radio'),
-(2, 'Lesquels de ces personnages ne sont pas des Jedi', 1, 'checkbox'),
-(3, 'Dans La Revanche des Siths quel adversaire affronte Maitre Yoda', 1, 'selecteur'),
-(4, 'Quel est l\'annee de sorti du premier Star Wars', 1, 'text'),
-(5, 'Dans le film \"Pulp fiction\", ou meurt le personnage incarne par John Travolta ?', 2, 'radio'),
-(6, 'Parmi ces personnages, quels sont ceux qui meurent dans le film \"Django unchained\" ?', 2, 'checkbox'),
-(7, 'Quel est le 1er film realise par Quentin Tarantino ?', 2, 'select'),
-(8, 'Combien de films Quentin Tarantino a-t-il realise ?', 2, 'number');
+(1, 'Combien de Star Wars a realise Georges Lucas ? ', 1, '1'),
+(2, 'Lesquels de ces personnages ne sont pas des Jedi', 1, '2'),
+(3, 'Dans La Revanche des Siths quel adversaire affronte Maitre Yoda', 1, '3'),
+(4, 'Quel est l\'annee de sorti du premier Star Wars', 1, '4'),
+(5, 'Dans le film \"Pulp fiction\", ou meurt le personnage incarne par John Travolta ?', 2, '1'),
+(6, 'Parmi ces personnages, quels sont ceux qui meurent dans le film \"Django unchained\" ?', 2, '2'),
+(7, 'Quel est le 1er film realise par Quentin Tarantino ?', 2, '3'),
+(8, 'Combien de films Quentin Tarantino a-t-il realise ?', 2, '4');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `quizz`
+-- Table structure for table `quizz`
 --
 
-DROP TABLE IF EXISTS `quizz`;
-CREATE TABLE IF NOT EXISTS `quizz` (
-  `quizz_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Quizz Identifiant',
-  `quizz_name` varchar(255) NOT NULL COMMENT 'Quizz name',
-  PRIMARY KEY (`quizz_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `quizz` (
+  `quizz_id` int NOT NULL COMMENT 'Quizz Identifiant',
+  `quizz_name` varchar(255) NOT NULL COMMENT 'Quizz name'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `quizz`
+-- Dumping data for table `quizz`
 --
 
 INSERT INTO `quizz` (`quizz_id`, `quizz_name`) VALUES
@@ -124,50 +118,115 @@ INSERT INTO `quizz` (`quizz_id`, `quizz_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user identifiant',
+CREATE TABLE `user` (
+  `user_id` int NOT NULL COMMENT 'user identifiant',
   `user_last_name` varchar(255) NOT NULL COMMENT 'user last name',
   `user_first_name` varchar(255) NOT NULL COMMENT 'user first name',
   `user_adress` longtext COMMENT 'user physical adress',
   `user_phone` varchar(255) DEFAULT NULL COMMENT 'user phone',
   `user_birthdate` datetime DEFAULT NULL,
-  `user_password` varchar(255) NOT NULL COMMENT 'User Password',
-  PRIMARY KEY (`user_id`)
+  `user_password` varchar(255) NOT NULL COMMENT 'User Password'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user_answer`
+-- Table structure for table `user_answer`
 --
 
-DROP TABLE IF EXISTS `user_answer`;
-CREATE TABLE IF NOT EXISTS `user_answer` (
-  `user_answer_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'User answer identifiant',
-  `user_id` int(11) NOT NULL COMMENT 'user identifiant',
-  `answer_id` int(11) NOT NULL COMMENT 'answer_id',
-  `user_answer_date` timestamp NULL DEFAULT NULL COMMENT 'date of answer user',
-  PRIMARY KEY (`user_answer_id`),
-  KEY `user_id_fk` (`user_id`),
-  KEY `answer_id_fk` (`answer_id`)
+CREATE TABLE `user_answer` (
+  `user_answer_id` int NOT NULL COMMENT 'User answer identifiant',
+  `user_id` int NOT NULL COMMENT 'user identifiant',
+  `answer_id` int NOT NULL COMMENT 'answer_id',
+  `user_answer_date` timestamp NULL DEFAULT NULL COMMENT 'date of answer user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Contraintes pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `question`
+-- Indexes for table `answer`
+--
+ALTER TABLE `answer`
+  ADD PRIMARY KEY (`answer_id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`question_id`),
+  ADD KEY `question_quizz_id_fk` (`question_quizz_id`);
+
+--
+-- Indexes for table `quizz`
+--
+ALTER TABLE `quizz`
+  ADD PRIMARY KEY (`quizz_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_answer`
+--
+ALTER TABLE `user_answer`
+  ADD PRIMARY KEY (`user_answer_id`),
+  ADD KEY `user_id_fk` (`user_id`),
+  ADD KEY `answer_id_fk` (`answer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `answer`
+--
+ALTER TABLE `answer`
+  MODIFY `answer_id` int NOT NULL AUTO_INCREMENT COMMENT 'answer identifier', AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `question_id` int NOT NULL AUTO_INCREMENT COMMENT 'question_identification', AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `quizz`
+--
+ALTER TABLE `quizz`
+  MODIFY `quizz_id` int NOT NULL AUTO_INCREMENT COMMENT 'Quizz Identifiant', AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'user identifiant';
+
+--
+-- AUTO_INCREMENT for table `user_answer`
+--
+ALTER TABLE `user_answer`
+  MODIFY `user_answer_id` int NOT NULL AUTO_INCREMENT COMMENT 'User answer identifiant';
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `question`
 --
 ALTER TABLE `question`
   ADD CONSTRAINT `question_quizz_id_fk` FOREIGN KEY (`question_quizz_id`) REFERENCES `quizz` (`quizz_id`);
 
 --
--- Contraintes pour la table `user_answer`
+-- Constraints for table `user_answer`
 --
 ALTER TABLE `user_answer`
   ADD CONSTRAINT `answer_id_fk` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`answer_id`),
