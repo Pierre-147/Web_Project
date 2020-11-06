6f1a528768ccd482ddc4a2c26d7ddfef8b14da82
Michael <michael.gross@student.yncrea.fr>
2020-11-06 16:30:25 +0100

    correction merge

--- vues/createAccount.php
+++ vues/createAccount.php
@@ -6,39 +6,10 @@
     <link href='css/homePage.css' rel='stylesheet'>
 </head>
 
-<<<<<<< HEAD:vues/createAccount.php
 <div id="content-wrap">
     <div class='formulaire'>
         <div class='Creation'>
             <h1> Créez votre compte !</h1>
-=======
-
-<!DOCTYPE html>
-<html>
-    <head>           
-        <link href='../css/CACss.css' rel='stylesheet'>
-        <link href='../css/body.css' rel='stylesheet'>
-        <link href='../css/HomePage.css' rel='stylesheet'>
-
-    </head>
-    <body>
-        <?php include('homepage.php') ?>
-        <div class='formulaire'>
-
-            <div class='Creation'>
-                <h1> Créez votre compte !</h1>
-                
-                <form method="post" action="CreateAccount.php">
-                    <br />
-                    <label> Pseudo : </label>
-                    <input type="text" placeholder="Pseudo"/><br /><br />
-                    <label> E-Mail : </label>
-                    <input type="mail" placeholder="e-mail"/><br /><br />
-                    <label> Mot de passe :</label>
-                    <input type="password" placeholder="mot de passe"/><br /><br />
-                    <label> Confirmer votre mot de passe : </label>
-                    <input type="password" placeholder="confirmer votre mot de passe"/><br /><br />
->>>>>>> Pierre:vues/CreateAccount.php
             
             <form method="post" action="createAccount.php">
                 <br />
