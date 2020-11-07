6893dc9a5f26279737accb94c11a4ed5ce51cea4
Michael <michael.gross@student.yncrea.fr>
2020-11-07 17:01:12 +0100

    maj

--- controler/trait.php
+++ controler/trait.php
@@ -6,13 +6,16 @@
         $response = $database->query("SELECT user_password,user_first_name FROM user WHERE user_mail='$user_email'");
         $pass=$response->fetch();
         
-        if($pass[0]==$password){
+        if($pass[0]===$password){
             $_SESSION["username"]=$pass[1];
             echo('connecte');
+            header("Location:index.php?page=account");
             
         }
-       
-        header("Location:index.php?page=account")
+        else
+        {
+            header("Location:index.php?page=createAccount");
+        }
         
         
 
